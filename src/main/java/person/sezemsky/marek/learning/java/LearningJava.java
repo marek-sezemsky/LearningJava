7af826049b681b5f87a0152132cb3e1b489ba84f
Marek Sezemský <marek.sezemsky@gmail.com>
2019-02-17 20:48:54 +0100

    Update LearningJava.java

--- src/main/java/person/sezemsky/marek/learning/java/LearningJava.java
+++ src/main/java/person/sezemsky/marek/learning/java/LearningJava.java
@@ -15,73 +15,107 @@ import org.fusesource.jansi.AnsiConsole;
 
 public class LearningJava {
 
+    static class Phase {
+
+        private String key;
+        private String title;
+        private long start;
+
+        public Phase(String key, String title) {
+            this.key = key;
+            this.title = title;
+            this.start = System.currentTimeMillis();
+
+            String msg = String.format("*** %s INIT %s", key, title);
+
+            LOG.debug(msg);
+            System.out.println(Chalk.on(msg).bold().white());
+        }
+
+        public void end() {
+            long duration = System.currentTimeMillis() - this.start;
+
+            String msg = String.format(
+                    "*** %s DONE in %,d [ms] : %s",
+                    this.key, duration, this.title);
+
+            LOG.debug(msg);
+            System.out.println(Chalk.on(msg).bold().white());
+        }
+
+    }
+
     private static final Logger LOG = LogManager.getLogger(LearningJava.class);
-
-    private static final String DEFAULT_ENCODING = "UTF-8";
     private static final long DEFAULT_SEED = 42;
-
-    private static final long CZECHS = 10_000;  // 10_000_000
+    private static final long CZECHS = 12_000_000;
 
     private static RandomPersonGenerator rpg;
 
-    public LearningJava() {
-    }
+    private static Phase totalElapsed;
+    private static Phase ph;
 
-    private static void header(String msg) {
-        LOG.info(msg);
-        System.out.println("\n\n##-- " + msg);
-    }
-
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        LOG.debug("Initiating startup sequence ..");
-        System.out.println(Chalk.on(
-                "\nBOOTUP Loading version 701.12 UPG").white().bold());
-
+    private static void phaseSetEncoding() throws UnsupportedEncodingException {
         try {
-            String encoding = System.getProperty("file.encoding", DEFAULT_ENCODING);
+            String encoding = System.getProperty("file.encoding");
             boolean autoFlush = true;
-            LOG.info("Using output encoding " + encoding);
+            LOG.info("Set System.out encoding to " + encoding);
             System.setOut(new PrintStream(System.out, autoFlush, encoding));
         } catch (RuntimeException e) {
             LOG.error("FUCK UP: ", e);  // lolwut
         }
+    }
 
-        System.out.println(Chalk.on(
-                "\nSYSCHK ...").blue().bold());
-
+    private static void phaseSystemCheck() {
         Runtime rt = Runtime.getRuntime();
-        long cpus = rt.availableProcessors();
-        long free = rt.freeMemory() >> 20;
-        long max = rt.maxMemory() >> 20;
-        long total = rt.totalMemory() >> 20;
-        System.out.println(String.format(
-                "Runtime reports %,d CPU + ( %,d MB FREE / %,d MB MAX / %,d MB TOTAL ) memory",
-                cpus, free, max, total));
+        String msg = String.format(
+                "Runtime reports %,d CPU + ( %,d MB FREE / %,d MB MAX / %,d MB TOTAL ) MEMORY",
+                rt.availableProcessors(),
+                rt.freeMemory() >> 20,
+                rt.maxMemory() >> 20,
+                rt.totalMemory() >> 20);
+        
+        LOG.debug(msg);
+        System.out.println(msg);
+    }
 
-        LOG.debug("System ready");
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+        LOG.debug("Initiating startup sequence ..");
         System.out.println(Chalk.on(
-                "SYSCHK DONE\nSYSTEM READY").green().bold());
+                "SYSTEM BOOT Loading version 701.12 UPG").white().bold());
 
-        header("Obtain generator");
+        ph = new Phase("SETENC", "Set Encoding engine");
+        phaseSetEncoding();
+        ph.end();
+
+        ph = new Phase("SYSSTS", "System status");
+        phaseSystemCheck();
+        ph.end();
+
+        ph = new Phase("SYSRDY", "SYSTEM READY");
+
+        totalElapsed = new Phase("TOTELA", "Total Elapsed");
         rpg = new RandomPersonGenerator(new Random(DEFAULT_SEED));
         LOG.debug("Generator: " + rpg);
 
-        header("Generate 10 randoms");
+        ph = new Phase("GEN10R", "Generate 10 randoms");
         for (int i = 0; i < 10; i++) {
             Person p = rpg.getRandomPerson();
             LOG.debug("Got " + p);
-            System.out.println(String.format("%d %s", i, p));
+            System.out.println(String.format("%d. %s", i + 1, p));
         }
+        ph.end();
 
-        header("Generate bulk randoms");
+        ph = new Phase("GENBLK", String.format("Generate %,d randoms in bulk", CZECHS));
         long startMilis = System.currentTimeMillis();
         List<Person> czechs = rpg.getBulk(CZECHS);
         long endMilis = System.currentTimeMillis();
         System.out.println(String.format(
                 "Generated %,d entries in %,d miliseconds",
                 czechs.size(), (endMilis - startMilis)));
+        ph.end();
 
-        header("Calculating collisions");
+        ph = new Phase("CALCOL", "Calculate collisions");
         Map<String, Long> map = new HashMap<String, Long>();
         long totalCollisions = 0L;
 
@@ -90,15 +124,16 @@ public class LearningJava {
             long timesSeen = map.getOrDefault(key, 0L);
 
             if (timesSeen > 0) {
-                System.out.println(String.format(
+                LOG.debug(String.format(
                         "Collision: Person %s already seen %d times.", p, timesSeen));
                 totalCollisions += 1;
             }
             map.put(key, timesSeen + 1);
         }
-        System.out.println("Found " + totalCollisions + " collisions.")
+        System.out.println(String.format("Found %,d collisions.", totalCollisions);
+        ph.end();
 
-        header("DONE");
+        totalElapsed.end();
 
     }
 }
