0232a7a6a136227010ee306a82cde95dba20a6a0
Marek Sezemský <marek.sezemsky@gmail.com>
2020-11-13 00:27:45 +0100

    HelloFX runner

--- src/main/java/org/example/sezemsky/HelloFX.java
+++ src/main/java/org/example/sezemsky/HelloFX.java
@@ -17,8 +17,12 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        StackPane stack = new StackPane();
+        
+        stack.getChildren().addAll(label);
+        Scene scene = new Scene(stack, 640, 480);
+        
         stage.setScene(scene);
         stage.show();
     }
